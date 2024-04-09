56e4ed64a63f9bd3f37411006586a558670d8210
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-04-09 16:00:45 +0200

    inizial app

---
+++ src/main/java/bkendw5/w5d2/W5D2Application.java
@@ -0,0 +1,13 @@
+package bkendw5.w5d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W5D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W5D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/bkendw5/w5d2/W5D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bkendw5.w5d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W5D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
